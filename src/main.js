a4b3ea62b6d17ec1f2f287ea9acac1ecd8f33c0c
Claudio De Martino <c.demartino1@studenti.unipi.it>
2022-08-04 12:13:58 +0200

    Deleted bootstrap-vue due to incompatibility with Vue3

--- src/main.js
+++ src/main.js
@@ -1,19 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 createApp(App).mount('#app');
-import Vue from 'vue';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import { createPopper } from '@popperjs/core';
-
-
-// Import Bootstrap and BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin);
 
 const popcorn = document.querySelector('#popcorn');
 const tooltip = document.querySelector('#tooltip');
